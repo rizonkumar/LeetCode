72c639c8462b34ca5a2ec264d05bc879924c8a1e
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-01-27 17:34:25 +0530

    Time: 2 ms (55.26%), Space: 41.4 MB (7.13%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,9 +1,15 @@
 class Solution {
     public int mySqrt(int x) {
-        long y = 0;
-        while(y * y <= x){
-            y++;
+        long s = 0, e = x, ans = 0;
+        while(s <=e){
+            long m = s+(e-s)/2;
+            if(m * m <= x){
+                ans = m;
+                s = m + 1;
+            } else {
+                e = m - 1;
+            }
         }
-        return (int) y - 1;
+        return (int) ans;
     }
 }
