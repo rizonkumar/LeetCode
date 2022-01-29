94e2c80247295855c41d5db5edeb270015c2a219
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-01-29 20:24:18 +0530

    Time: 2 ms (57.13%), Space: 42.6 MB (5.86%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
+        // Time Complexity =>  O(26 + N + M)
         int freq1[] = new int[26];
-        int freq2[] = new int[26];
         // a - > 0
         // b - > 1
         // c - > 2
         // z - > 25
+        
+      
         for(int i = 0; i < word1.length(); i++){
             // to find the ascii value 
             freq1[word1.charAt(i) - 'a']++;
         }
         
         for(int i = 0; i < word2.length(); i++){
-            freq2[word2.charAt(i) - 'a']++;
+            freq1[word2.charAt(i) - 'a']--;
         }
         
         for(int i = 0; i < 26; i++){
-            if(Math.abs(freq1[i] - freq2[i]) > 3){
-                return false;
-            }
+            if(Math.abs(freq1[i]) > 3)    return false;
         }
         return true;
-
     }
 }
