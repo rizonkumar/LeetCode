492d47a594b4d7b024ecbb1ada334c0204fea9e9
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-01-30 10:57:57 +0530

    Time: 1 ms (76.47%), Space: 64.1 MB (15.06%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,17 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k % n;  // if n times it wrote then same array
-        int[] ans = new int[n];
-        for(int i = n - k; i < n; i++){
-            ans[i-(n - k)] = nums[i];
-        }
-        for(int i = k; i < n; i++){
-            ans[i] = nums[i - k];
-        }
-        // copy the array
-        for(int i = 0; i < n; i++){
-            nums[i] = ans[i];
+        k = k % n;
+        flip(nums, 0, n-1);
+        flip(nums, 0, k-1);
+        flip(nums, k, n - 1);
+    }
+    
+    public void flip(int[] arr, int s, int e){
+        while(s < e){
+            int temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+            s++;
+            e--;
         }
     }
 }
