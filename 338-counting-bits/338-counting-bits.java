c1019ca62476561fc1cf925e43612a8f16d494f0
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-03-01 17:58:39 +0530

    Time: 1 ms (99.97%), Space: 45.9 MB (55.37%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,19 +1,22 @@
 class Solution {
     public int[] countBits(int n) {
-    // create one result array,
-    // & our array size is n + 1 because we have to cover 0 as well
-        int[] R = new int[n+1];
-        if(n==0) return R;
+        // create one ans array,
+        // & our array size is n + 1 because we have to cover 0 as well
+        int[] ans = new int[n+1];
+        // base condition
+        if(n == 0 ) return ans;
         
         // run a loop to store all the values
         for(int i=1;i<n+1;i++) {
-            if(i%2 == 0) { // number is even
-                R[i] = R[i/2];
+            if(i%2 == 0) { 
+            // number is even
+                ans[i] = ans[i/2];
             }
-            else { // is odd
-                R[i] = R[i/2] + 1;
+            else { 
+            // is odd
+                ans[i] = ans[i/2] + 1;
             }
         }
-        return R;
+        return ans;
     }
 }
