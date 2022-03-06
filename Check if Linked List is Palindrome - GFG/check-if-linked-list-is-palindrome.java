f9e117f43f8bc6add914fd45b8df4e89a76c5586
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-03-06 21:51:34 +0530

    Added solution - LeetHub

---
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.java
@@ -0,0 +1,154 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Is_LinkedList_Palindrom
+{
+	 Node head;  
+	 Node tail;
+	
+	/* Function to print linked list */
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+ 
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) 
+	{
+
+		if (head == null) 
+		{
+			head = node;
+			tail = node;
+		} else 
+		{
+		    tail.next = node;
+		    tail = node;
+		}
+    }
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		 
+		while(t>0)
+        {
+			int n = sc.nextInt();
+			//int k = sc.nextInt();
+			Is_LinkedList_Palindrom llist = new Is_LinkedList_Palindrom();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            Node tail = head;
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+			    tail.next = new Node(a);
+			    tail = tail.next;
+			}
+			
+			Solution g = new Solution();
+			if(g.isPalindrome(head) == true)
+			    System.out.println(1);
+		    else
+			    System.out.println(0);
+			t--;
+		}
+		
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+/* Structure of class Node is
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+
+class Solution {
+public Node reverseList(Node head){
+        
+        // edge case
+        if(head == null){
+            return head;
+        }
+        
+        Node prev = null;
+        Node present = head;
+        Node next = present.next;
+        
+        while(present != null){
+            present.next = prev;
+            prev = present;
+            present = next;
+            if(next != null){
+                next = next.next;
+            }
+        }
+    return prev;
+    }
+    
+    // middle of the linked list
+    public Node middleNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+    public boolean isPalindrome(Node head) {
+        Node mid = middleNode(head);
+        Node headSecond = reverseList(mid);
+        Node rereverseHead = headSecond;
+        
+        //compare both the half
+        while(head != null && headSecond != null){
+            if(head.data != headSecond.data){
+                break;
+            }
+            head = head.next;
+            headSecond = headSecond.next;
+        }
+        reverseList(rereverseHead);
+        
+        if(head == null || headSecond == null)
+            return true;
+        return false;
+    }
+}
+
