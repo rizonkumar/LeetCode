ac15d64fc45b3dec0e8bc6f416f1a6a015ee6e08
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-03-14 12:05:24 +0530

    Time: 5 ms (84.92%), Space: 44.3 MB (26.08%) - LeetHub

--- 71-simplify-path/71-simplify-path.java
+++ 71-simplify-path/71-simplify-path.java
@@ -1,17 +1,17 @@
-// Stack Solution
+// Deque Solution
 
 class Solution {
     public String simplifyPath(String path) {
-        Stack<String> s = new Stack<>();
+        Deque<String> s = new LinkedList<>();
         StringBuilder res = new StringBuilder();
         String[] p = path.split("/");
         
         for(int i=0; i<p.length; i++){
             // two condition now pop and pushing
-            //popping
-            //whenever two dots present we pop it and stack should't be emptp
+            //polling
+            //whenever two dots present we pop it and queue should't be empty
             if(!s.isEmpty() && p[i].equals("..")) 
-                s.pop();
+                s.poll();
             
             //pushing
             //whenever empty string and when there is not single dot or double dot
@@ -20,10 +20,9 @@ class Solution {
         }
         // base case
         if(s.isEmpty()) return "/";
-        //if not empty iterate over the stack and append the element in string builder in res.
         while(!s.isEmpty()){
-            // appending in front rather than back because stack would pop the last element first but we need in FIFO order.
-            res.insert(0, s.pop()). insert(0, "/");
+            // only change in while for Dequeue
+            res.append("/").append(s.pollLast());
         }
         return res.toString();
     }
