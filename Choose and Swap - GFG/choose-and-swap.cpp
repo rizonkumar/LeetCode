c97a25b04cda5999e2e949a93dc3870a46da64d6
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-03-20 08:35:19 +0530

    Added solution - LeetHub

---
+++ Choose and Swap - GFG/choose-and-swap.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    string swap(string &s, char a, char b)
+    {
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]==a) s[i]=b;
+            else if(s[i]==b) s[i]=a;
+        }
+        return s;
+    }
+public:
+    string chooseandswap(string a){
+        vector<int> first(26, 100001);
+        for(int i=a.length()-1;i>=0;i--)
+            first[a[i] -'a'] = i;
+    
+        for(int i=0;i<26;i++)
+        {
+            if(first[i]<100001){
+                int minn = 100001;
+                for(int j=i+1;j<26;j++)
+                    minn = min(minn, first[j]);
+    
+                if(minn<first[i])
+                    return swap(a, 'a'+i, a[minn]);
+            }
+        }
+        return a;
+    }
+    
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    Solution obj;
+	int t;
+	cin >> t;
+	while(t--)
+	{	
+	    string a;
+		cin >> a;
+		cout << obj.chooseandswap(a) << endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
