cf921fbde0f1e14c248341377bfd78d63208ff5a
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-03-23 08:11:58 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (30.28%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.java
+++ 991-broken-calculator/991-broken-calculator.java
@@ -1,12 +1,15 @@
+// Iterative Approach
 class Solution {
     public int brokenCalc(int startValue, int target) {
-        if(startValue >= target)
-            return startValue - target;
-        
-        // as we are trying to move backward we do divide and addition
-        if(target%2 == 0){
-            return 1 + brokenCalc(startValue, target/2);
+        int res = 0;
+        while(target > startValue){
+            res++;
+            if(target%2 == 0){
+                target/=2;
+            } else {
+                target++;
+            }
         }
-        return 1 + brokenCalc(startValue, target + 1);
+        return res + (startValue - target);
     }
 }
