8cffec896afb471e133d2a70ad83ec6e2d5d1ff4
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-03-24 18:54:23 +0530

    Time: 8 ms (98.38%), Space: 68.5 MB (5.39%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -1,6 +1,19 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
+        
+        //Count Sort
+        int[] count = new int[limit+1];
+        for(int p: people){
+            count[p]++;
+        }
+        
+        int index = 0;
+        for(int val = 1; val <= limit; val++){
+            while(count[val]-->0){
+                people[index++]=val;
+            }
+        }
+        
         int left = 0;
         int right = people.length - 1;
         int boats = 0;
