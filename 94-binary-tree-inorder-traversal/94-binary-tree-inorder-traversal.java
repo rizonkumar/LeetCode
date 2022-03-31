88e0651252e2e92e10a613dd2ec42df8541032ad
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-03-31 19:03:15 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (65.62%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -1,57 +1,31 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Morris Traversal Inorder
 class Solution {
-   /*
-   // Recursion
-   
-    private void dfs(TreeNode node, List<Integer> inorder){
-        if(node == null) return;
-        
-        // InOrder -> Left Root Right
-        dfs(node.left, inorder);
-        inorder.add(node.val);
-        dfs(node.right, inorder);
-    }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> inorder = new ArrayList<Integer>(); 
-        dfs(root, inorder);
-        return inorder; 
-    }
-    
-    */
-    
-    // Iterative
-    
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> inorder = new ArrayList<Integer>();
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        TreeNode node = root;
-        while(true){
-            if(node != null){
-                stack.push(node);
-                node = node.left;
-            } else{ // if it is null whtever is at top is our inorder{
-                 if(stack.isEmpty()){
-                     break;
-                 }
-            node = stack.pop();
-            inorder.add(node.val);
-            node = node.right;
+        
+        TreeNode cur = root; 
+        while(cur != null) {
+            if(cur.left == null) {
+                inorder.add(cur.val); 
+                cur = cur.right; 
+            }
+            else {
+                TreeNode prev = cur.left; 
+                while(prev.right != null && prev.right != cur) {
+                    prev = prev.right; 
+                }
+                
+                if(prev.right == null) {
+                    prev.right = cur;
+                    cur = cur.left; 
+                }
+                else {
+                    prev.right = null; 
+                    inorder.add(cur.val); 
+                    cur = cur.right; 
+                }
             }
         }
-    return inorder;
+        return inorder; 
     }
 }
