513e3c5d64146e7a2c5f3b5e58a2a5aaec8ff746
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-04-01 09:13:51 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (86.41%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -1,28 +1,23 @@
 // TC - O(N) 
-// SC - O(N) 
-// Iterative 
+// SC - O(1) 
+// Morris Traversal
 class Solution {
-
     public void flatten(TreeNode root) {
-        if(root == null) return; 
-        
-        Deque<TreeNode> st = new ArrayDeque<>(); 
-        st.push(root); 
-        while(!st.isEmpty()) {
-            TreeNode cur = st.peek();
-            st.pop();
-            
-            if(cur.right != null) {
-                st.push(cur.right); 
-            }
-            if(cur.left != null) {
-                st.push(cur.left); 
-            }
-            if(!st.isEmpty()) {
-                cur.right = st.peek(); 
-            }
-            cur.left = null;
-        }
-        
+        TreeNode cur = root;
+		while (cur != null)
+		{
+			if(cur.left != null)
+			{
+				TreeNode pre = cur.left;
+				while(pre.right != null)
+				{
+					pre = pre.right;
+				}
+				pre.right = cur.right;
+				cur.right = cur.left;
+				cur.left = null;
+			}
+			cur = cur.right;
+		}
     }
 }
