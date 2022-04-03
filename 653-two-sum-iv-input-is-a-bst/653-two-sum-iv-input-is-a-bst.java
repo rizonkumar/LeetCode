0f18f0e2dd4ee3f22f9707e7bf634a188dcbe40c
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-04-03 17:31:36 +0530

    Time: 6 ms (59.55%), Space: 50.4 MB (71.90%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,53 @@
+public class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+    // reverse -> true -> before
+    // reverse -> false -> next
+    boolean reverse = true;      // before 
+    
+    public BSTIterator(TreeNode root, boolean isReverse) {
+        reverse = isReverse; 
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode tmpNode = stack.pop();
+        if(reverse == false) pushAll(tmpNode.right);
+        else pushAll(tmpNode.left); 
+        return tmpNode.val;
+    }
+    
+    private void pushAll(TreeNode node) {
+        while(node != null) {
+             stack.push(node);
+             if(reverse == true) {
+                 node = node.right; 
+             } else {
+                 node = node.left; 
+             }
+        }
+    }
+}
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null) return false; 
+        // next
+        BSTIterator l = new BSTIterator(root, false); 
+        // for before
+        BSTIterator r = new BSTIterator(root, true); 
+        
+        int i = l.next(); 
+        int j = r.next(); 
+        while(i<j) {
+            if(i + j == k) return true; 
+            else if(i + j < k) i = l.next(); 
+            else j = r.next(); 
+        }
+        return false; 
+    }
+}
