e16c0a4959cd275c54041fad8f4da76e777181de
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-04-14 09:35:46 +0530

    Time: 0 ms (100.00%), Space: 43 MB (81.70%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -1,21 +1,24 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-        while(root != NULL && root->val != val){
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null && root.val != val){
             // if the root value is less than val then move left else move right
-            root = val < root->val ? root->left : root->right;
+            root = val < root.val ? root.left : root.right;
         }
         return root;
     }
-};
+}
