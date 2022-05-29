9897feb409d83706505408ee7511a90280799574
Rizon Kumar Rahi <78218974+rizonkumar@users.noreply.github.com>
2022-05-29 21:33:49 +0530

    Time: 17 ms (56.35%), Space: 46.5 MB (57.27%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -4,6 +4,7 @@ class Solution {
             return 0;
         int len = words.length;
         int[] value = new int[len];
+            
         for (int i = 0; i < len; i++) {
             String tmp = words[i];
             value[i] = 0;
@@ -11,12 +12,14 @@ class Solution {
                 value[i] |= 1 << (tmp.charAt(j) - 'a');
             }
         }
+            
         int maxProduct = 0;
         for (int i = 0; i < len; i++)
             for (int j = i + 1; j < len; j++) {
                 if ((value[i] & value[j]) == 0 && (words[i].length() * words[j].length() > maxProduct))
                     maxProduct = words[i].length() * words[j].length();
             }
+            
         return maxProduct;
     }
 }
