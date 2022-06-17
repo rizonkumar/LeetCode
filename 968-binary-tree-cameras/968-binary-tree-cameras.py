7f8f78b8229c68f7ed3636bc416ae4ef2af15827
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-06-17 11:39:27 +0530

    Time: 71 ms (5.26%), Space: 13.8 MB (77.19%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.py
+++ 968-binary-tree-cameras/968-binary-tree-cameras.py
@@ -1,17 +1,18 @@
 class Solution(object):
     def minCameraCover(self, root):
-        self.ans = 0
-        covered = {None}
+        def solve(node):
+            # 0: Strict ST; All nodes below this are covered, but not this one
+            # 1: Normal ST; All nodes below and incl this are covered - no camera
+            # 2: Placed camera; All nodes below this are covered, plus camera here
 
-        def dfs(node, par = None):
-            if node:
-                dfs(node.left, node)
-                dfs(node.right, node)
+            if not node: return 0, 0, float('inf')
+            L = solve(node.left)
+            R = solve(node.right)
 
-                if (par is None and node not in covered or
-                        node.left not in covered or node.right not in covered):
-                    self.ans += 1
-                    covered.update({node, par, node.left, node.right})
+            dp0 = L[1] + R[1]
+            dp1 = min(L[2] + min(R[1:]), R[2] + min(L[1:]))
+            dp2 = 1 + min(L) + min(R)
 
-        dfs(root)
-        return self.ans
+            return dp0, dp1, dp2
+
+        return min(solve(root)[1:])
