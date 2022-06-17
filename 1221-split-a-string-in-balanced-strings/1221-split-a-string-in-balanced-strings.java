386bb313e39a18a0041aa43a1290a15aca6ce0f1
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-06-17 21:02:33 +0530

    Time: 1 ms (50.03%), Space: 41.9 MB (48.01%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -1,3 +1,11 @@
+/*
+Greedily split the string, and with the counting
+L +1
+R -1
+
+when the counter is reset to 0, we get one balanced string.
+*/
+
 class Solution {
     public int balancedStringSplit(String s) {
         int count = 0, ans = 0;
