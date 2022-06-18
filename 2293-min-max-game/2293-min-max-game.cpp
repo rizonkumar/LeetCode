fc66b642171b3abe75e407bf9bdaecc7e816b752
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-06-18 16:41:28 +0530

    Time: 16 ms (22.49%), Space: 9.7 MB (95.93%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.cpp
+++ 2293-min-max-game/2293-min-max-game.cpp
@@ -1,19 +1,17 @@
-// Brute Force
+// Better Approach
 class Solution {
 public:
     int minMaxGame(vector<int>& nums) {
-        while(nums.size() > 1){
-            int n = nums.size();
-            vector<int> temp(n/2);
-             
+        int n = nums.size();
+        while(n > 1){
             for(int i = 0; i < n/2; i++){
                 if(i % 2 == 0){
-                    temp[i] = min(nums[2 * i], nums[2 * i + 1]);
+                    nums[i] = min(nums[2 * i], nums[2 * i + 1]);
                 } else {
-                    temp[i] = max(nums[2 * i], nums[2 * i + 1]);
+                    nums[i] = max(nums[2 * i], nums[2 * i + 1]);
                 }
             }
-            nums = temp;
+            n /= 2;
         }
         return nums[0];
     }
