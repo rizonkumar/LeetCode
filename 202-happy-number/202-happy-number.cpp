12f7c475169e11859bac66eb81c0fb1f6f6aeb2b
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-06-21 10:31:08 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (93.83%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -3,6 +3,8 @@ public:
     // Detect Cycle using  Floyd Cycle detection Algorithm(it is using 2 pointers(slow & fast pointer))
     // if we detect cycle then it is not a happy number otherwise yes
     // it can also solved by using HashMap or Set
+    
+    // finding the square of the digit
     int solve(int n) {
         int sum = 0;
         while(n > 0) {
@@ -12,7 +14,7 @@ public:
         }
         return sum;
     }
-    
+    //  Floyd's Cycle-Finding Algorithm
     bool isHappy(int n) {
         int slow = n, fast = n;
         do {
