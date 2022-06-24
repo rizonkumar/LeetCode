e3a265173f7dce227f257a4076a5811dff3e91f3
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-06-24 10:26:27 +0530

    Time: 2 ms (22.73%), Space: 50.7 MB (45.20%) - LeetHub

---
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        
+        if(mat.length * mat[0].length != r * c){
+            return mat;
+        }   
+        // if the equality is meet
+        int [][] newMatrix = new int[r][c];
+        
+        int sR = 0, sC = 0;
+        
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[0].length; j++){
+                if(sC == c){
+                    sR++;
+                    sC = 0;
+                }
+                newMatrix[sR][sC] = mat[i][j];
+                sC++;
+            }
+        }
+        return newMatrix;
+    }
+}
