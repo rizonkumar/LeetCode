8b42abca0db3e44fd3095d8bd6c52535e3965aa2
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-06-25 10:13:54 +0530

    Time: 2 ms (57.10%), Space: 6.4 MB (55.18%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,16 +1,13 @@
-// Two Pointer
+// One Pointer
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
-        int i = 0, j = 0;
-        
         string res = "";
-        while(i < n || j < m){
-            if(i < n)
-                res.push_back(word1[i++]);
-            if(j < m)
-                res.push_back(word2[j++]);
+        for(int i = 0; i < word1.size() || i < word2.size(); i++){
+            if(i < word1.size())
+                res.push_back(word1[i]);
+            if(i < word2.size())
+                res.push_back(word2[i]);
         }
         return res;
     }
