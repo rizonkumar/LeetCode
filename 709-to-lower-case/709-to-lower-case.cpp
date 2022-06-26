3bece83c76037abccce6728df5c08855b892b4ec
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-06-26 09:54:03 +0530

    Time: 7 ms (6.03%), Space: 6.1 MB (51.77%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(char &c : s){
-            if(c >='A' && c <='Z') c+=32;
+            // ASCII value of upper is 65 to 90, lower is 97 // So c+= 32 // 65 + 32 = 97
+            if(c >='A' && c <='Z') c+=32;   
         }
         
         return s;
