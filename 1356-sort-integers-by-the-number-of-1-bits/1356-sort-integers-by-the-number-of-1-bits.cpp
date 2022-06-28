1b3f4c058375d3df778c35c3dc9be285138835a4
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-06-28 16:37:57 +0530

    Time: 7 ms (93.40%), Space: 10.2 MB (48.55%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    
+   // __builtin_popcount(x): This function is used to count the number of one’s(set bits) in an integer. 
+    static bool compare(const int& a, const int& b){
+        int c1 = __builtin_popcount(a);
+        int c2 = __builtin_popcount(b);
+        if(c1 == c2)
+            return a < b;
+        return c1 < c2;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        std::sort(arr.begin(),arr.end(), compare);
+        return arr;
+    }
+};
