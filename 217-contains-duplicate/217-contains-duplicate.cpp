2219840fe9cccdb8a5d787cc0cb1b9a6e0b9e38b
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-06-28 16:38:17 +0530

    Time: 112 ms (78.60%), Space: 46.6 MB (91.66%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        int n = nums.size();
+        if(n<=1) return false;
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 1; i < n; i++){
+            
+            if(nums[i] == nums[i-1])
+                return true;
+        }
+        return false;
+        
+    }
+};
