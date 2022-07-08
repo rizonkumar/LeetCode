86336a6696e0f7a6cacacd3589990c0a2a7fc1e6
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-07-08 11:42:57 +0530

    Time: 36 ms (70.00%), Space: 49.2 MB (45.00%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.java
+++ 1473-paint-house-iii/1473-paint-house-iii.java
@@ -1,51 +1,64 @@
 class Solution {
-    // Assign the size as per maximum value for different params
-    Integer[][][] memo = new Integer[100][100][21];
     // Maximum cost possible plus 1
     final int MAX_COST = 1000001;
     
-    int findMinCost(int[] houses, int[][] cost, int targetCount, int currIndex,
-                    int neighborhoodCount, int prevHouseColor) {
-        if (currIndex == houses.length) {
-            // If all houses are traversed, check if the neighbor count is as expected or not
-            return neighborhoodCount == targetCount ? 0 : MAX_COST;
+    public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
+        int[][][] memo = new int[m][target + 1][n];
+      
+        // Initialize memo array
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j <= target; j++) {
+                Arrays.fill(memo[i][j], MAX_COST);
+            }
         }
-        
-        if (neighborhoodCount > targetCount) {
-            // If the neighborhoods are more than the threshold, we can't have target neighborhoods
-            return MAX_COST;
-        }
-        
-        // We have already calculated the answer so no need to go into recursion
-        if (memo[currIndex][neighborhoodCount][prevHouseColor] != null) {
-            return memo[currIndex][neighborhoodCount][prevHouseColor];
-        }
-        
-        int minCost = MAX_COST;
-        // If the house is already painted, update the values accordingly
-        if (houses[currIndex] != 0) {
-            int newNeighborhoodCount = neighborhoodCount + (houses[currIndex] != prevHouseColor ? 1 : 0);
-            minCost = 
-                findMinCost(houses, cost, targetCount, currIndex + 1, newNeighborhoodCount, houses[currIndex]);
-        } else {
-            int totalColors = cost[0].length;
             
-            // If the house is not painted, try every possible color and store the minimum cost
-            for (int color = 1; color <= totalColors; color++) {
-                int newNeighborhoodCount = neighborhoodCount + (color != prevHouseColor ? 1 : 0);
-                int currCost = cost[currIndex][color - 1] 
-                    + findMinCost(houses, cost, targetCount, currIndex + 1, newNeighborhoodCount, color);
-                minCost = Math.min(minCost, currCost);
+        // Initialize for house 0, neighborhoods will be 1
+        for (int color = 1; color <= n; color++) {
+            if (houses[0] == color) {
+                // If the house has same color, no cost
+                memo[0][1][color - 1] = 0;
+            } else if (houses[0] == 0) {
+                // If the house is not painted, assign the corresponding cost
+                memo[0][1][color - 1] = cost[0][color - 1];
             }
         }
         
-        // Return the minimum cost and also storing it for future reference (memoization)
-        return memo[currIndex][neighborhoodCount][prevHouseColor] = minCost;
-    }
-    
-    public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        int answer = findMinCost(houses, cost, target, 0, 0, 0);
+        for (int house = 1; house < m; house++) {
+            for (int neighborhoods = 1; neighborhoods <= Math.min(target, house + 1); neighborhoods++) {
+                for (int color = 1; color <= n; color++) {
+                    // If the house is already painted, and color is different
+                    if (houses[house] != 0 && color != houses[house]) {
+                        // Cannot be painted with different color
+                        continue;
+                    }
+ 
+                    int currCost = MAX_COST;
+                    // Iterate over all the possible color for previous house
+                    for (int prevColor = 1; prevColor <= n; prevColor++) {
+                        if (prevColor != color) {
+                            // Decrement the neighborhood as adjacent houses has different color
+                            currCost = Math.min(currCost, memo[house - 1][neighborhoods - 1][prevColor - 1]);
+                        } else {
+                            // Previous house has the same color, no change in neighborhood count
+                            currCost = Math.min(currCost, memo[house - 1][neighborhoods][color - 1]);
+                        }
+                    }
+
+                    // If the house is already painted, cost to paint is 0
+                    int costToPaint = houses[house] != 0 ? 0 : cost[house][color - 1];
+                    memo[house][neighborhoods][color - 1] = currCost + costToPaint;
+                }
+            }
+        }
+        
+        int minCost = MAX_COST;
+        // Find the minimum cost with m houses and target neighborhoods
+        // By comparing cost for different color for the last house
+        for (int color = 1; color <= n; color++) {
+            minCost = Math.min(minCost, memo[m - 1][target][color - 1]);
+        }
+        
         // Return -1 if the answer is MAX_COST as it implies no answer possible
-        return answer == MAX_COST ? -1 : answer;
+        return minCost == MAX_COST ? -1 : minCost;
     }
 }
