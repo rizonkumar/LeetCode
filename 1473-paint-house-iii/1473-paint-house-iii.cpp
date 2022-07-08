55a8cde0c0490c27c425329620730c5a9745d643
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-07-08 11:43:23 +0530

    Time: 141 ms (48.40%), Space: 18.8 MB (22.07%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -4,21 +4,24 @@ public:
     const int MAX_COST = 1000001;
 
     int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
-        // Initialize memo array
-        vector<vector<vector<int>>> memo(m, vector<vector<int>>(target + 1, vector<int>(n, MAX_COST)));
-            
-        // Initialize for house 0, neighborhoods will be 1
+        // Initialize prevMemo array
+        vector<vector<int>> prevMemo(target + 1, vector<int>(n, MAX_COST));
+       
+        // Initialize for house 0, neighborhood will be 1
         for (int color = 1; color <= n; color++) {
             if (houses[0] == color) {
                 // If the house has same color, no cost
-                memo[0][1][color - 1] = 0;
+                prevMemo[1][color - 1] = 0;
             } else if (!houses[0]) {
                 // If the house is not painted, assign the corresponding cost
-                memo[0][1][color - 1] = cost[0][color - 1];
+                prevMemo[1][color - 1] = cost[0][color - 1];
             }
         }
         
         for (int house = 1; house < m; house++) {
+            // Initialize memo array
+            vector<vector<int>> memo(target + 1, vector<int>(n, MAX_COST));
+            
             for (int neighborhoods = 1; neighborhoods <= min(target, house + 1); neighborhoods++) {
                 for (int color = 1; color <= n; color++) {
                     // If the house is already painted, and color is different
@@ -32,25 +35,27 @@ public:
                     for (int prevColor = 1; prevColor <= n; prevColor++) {
                         if (prevColor != color) {
                             // Decrement the neighborhood as adjacent houses has different color
-                            currCost = min(currCost, memo[house - 1][neighborhoods - 1][prevColor - 1]);
+                            currCost = min(currCost, prevMemo[neighborhoods - 1][prevColor - 1]);
                         } else {
                             // Previous house has the same color, no change in neighborhood count
-                            currCost = min(currCost, memo[house - 1][neighborhoods][color - 1]);
+                            currCost = min(currCost, prevMemo[neighborhoods][color - 1]);
                         }
                     }
 
                     // If the house is already painted cost to paint is 0
                     int costToPaint = houses[house] ? 0 : cost[house][color - 1];
-                    memo[house][neighborhoods][color - 1] = currCost + costToPaint;
+                    memo[neighborhoods][color - 1] = currCost + costToPaint;
                 }
             }
+            // Update the table to have the current house results
+            prevMemo = memo;
         }
         
         int minCost = MAX_COST;
         // Find the minimum cost with m houses and target neighborhoods
         // By comparing cost for different color for the last house
         for (int color = 1; color <= n; color++) {
-            minCost = min(minCost, memo[m - 1][target][color - 1]);
+            minCost = min(minCost, prevMemo[target][color - 1]);
         }
         
         // Return -1 if the answer is MAX_COST as it implies no answer possible
