3df7e51c1bb044ebe8743ad17c5323fc9b934f48
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-07-12 09:36:06 +0530

    Time: 147 ms (58.03%), Space: 48 MB (16.32%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -1,41 +1,75 @@
 import java.util.HashMap;
-import java.util.Collections;
 
 class Solution {
-    public List<Integer> nums;
-    public int[] sums;
+
+    // The memoization cache to be used during recursion.
+    public HashMap<Pair<Integer, Integer>, Boolean> memo;
+
+    // Array containing our matchsticks.
+    public int[] nums;
+
+    // Possible side of our square depending on the total sum of all the matchsticks. 
     public int possibleSquareSide;
 
+    // Default constructor to initialise our memo cache.
     public Solution() {
-        this.sums = new int[4];
+        this.memo = new HashMap<Pair<Integer, Integer>, Boolean>();
     }
 
-    // Depth First Search function.
-    public boolean dfs(int index) {
+    // Main DP function.
+    public boolean recurse(Integer mask, Integer sidesDone) {
+        int total = 0;
+        int L = this.nums.length;
 
-        // If we have exhausted all our matchsticks, check if all sides of the square are of equal length
-        if (index == this.nums.size()) {
-            return sums[0] == sums[1] && sums[1] == sums[2] && sums[2] == sums[3];
-        }
+        // The memo key for this recursion
+        Pair<Integer, Integer> memoKey = new Pair(mask, sidesDone);
 
-        // Get current matchstick.
-        int element = this.nums.get(index);
-
-        // Try adding it to each of the 4 sides (if possible)
-        for(int i = 0; i < 4; i++) {
-            if (this.sums[i] + element <= this.possibleSquareSide) {
-                this.sums[i] += element;
-                if (this.dfs(index + 1)) {
-                    return true;
-                }
-                this.sums[i] -= element;
+        // Find out the sum of matchsticks used till now.
+        for(int i = L - 1; i >= 0; i--) {
+            if ((mask&(1 << i)) == 0) {
+                total += this.nums[L - 1 - i];
             }
         }
 
-        return false;
+        // If the sum if divisible by our square's side, then we increment our number of complete sides formed variable.
+        if (total > 0 && total % this.possibleSquareSide == 0) {
+            sidesDone++;
+        }
+
+        // Base case.
+        if (sidesDone == 3) {
+            return true;
+        }
+
+
+        // Return precomputed results
+        if (this.memo.containsKey(memoKey)) {
+            return this.memo.get(memoKey);
+        }
+
+        boolean ans = false;
+        int c = total / this.possibleSquareSide;
+
+        // Remaining vlength in the current partially formed side.
+        int rem = this.possibleSquareSide * (c + 1) - total;
+
+        // Try out all remaining options (that are valid)
+        for(int i = L - 1; i >= 0; i--) {
+            if (this.nums[L - 1 - i] <= rem && (mask&(1 << i)) > 0) {
+                if (this.recurse(mask ^ (1 << i), sidesDone)) {
+                    ans = true;
+                    break;
+                }
+            }
+        }
+
+        // Cache the computed results.
+        this.memo.put(memoKey, ans);
+        return ans;
     }
 
     public boolean makesquare(int[] nums) {
+
         // Empty matchsticks.
         if (nums == null || nums.length == 0) {
             return false;
@@ -48,14 +82,13 @@ class Solution {
             perimeter += nums[i];
         }
 
-        this.possibleSquareSide =  perimeter / 4;
-        if (this.possibleSquareSide * 4 != perimeter) {
+        int possibleSquareSide =  perimeter / 4;
+        if (possibleSquareSide * 4 != perimeter) {
             return false;
         }
 
-        // Convert the array of primitive int to ArrayList (for sorting).
-        this.nums = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        Collections.sort(this.nums, Collections.reverseOrder());
-        return this.dfs(0);
+        this.nums = nums;
+        this.possibleSquareSide = possibleSquareSide;
+        return this.recurse((1 << L) - 1, 0);
     }
 }
