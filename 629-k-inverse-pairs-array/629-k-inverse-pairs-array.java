67df5ada917dd36d918b53a5b39ed8bd7917ba06
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2022-07-17 10:30:10 +0530

    Time: 34 ms (61.45%), Space: 47.5 MB (72.29%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
@@ -3,8 +3,11 @@ public class Solution {
         int[][] dp = new int[n + 1][k + 1];
         int M = 1000000007;
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                if (j == 0)
+            for (int j = 0; j <= k && j <= i * (i - 1) / 2; j++) {
+                if (i == 1 && j == 0) {
+                    dp[i][j] = 1;
+                    break;
+                } else if (j == 0)
                     dp[i][j] = 1;
                 else {
                     int val = (dp[i - 1][j] + M - ((j - i) >= 0 ? dp[i - 1][j - i] : 0)) % M;
@@ -12,6 +15,6 @@ public class Solution {
                 }
             }
         }
-        return ((dp[n][k] + M - (k > 0 ? dp[n][k - 1] : 0)) % M);
+        return dp[n][k];
     }
 }
